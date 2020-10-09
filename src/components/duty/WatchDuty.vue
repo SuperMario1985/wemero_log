ff589bd4864c250e569ff2971594cf0ccd86decf
lileilei <lileilei@idengyun.com>
2020-10-09 14:04:13 +0800

    feat: 十月值班表

--- src/components/duty/WatchDuty.vue
+++ src/components/duty/WatchDuty.vue
@@ -19,7 +19,7 @@
     </div>
     <div class="main-content" v-loading="isLoading">
       <el-tabs type="border-card">
-        <el-tab-pane label="8月值班人员">
+        <el-tab-pane label="10月值班人员">
           <el-tabs class="duty-item" v-model="AugustValue" @tab-click="handleClick">
             <el-tab-pane
               v-for="(item,$index) in AugustDuty"
@@ -32,7 +32,7 @@
         <!-- <el-tab-pane label="9月值班人员">配置管理</el-tab-pane> -->
       </el-tabs>
       <el-tabs type="border-card">
-        <el-tab-pane label="8月自测人员">
+        <el-tab-pane label="10月自测人员">
           <el-tabs class="duty-item" v-model="AugustTestValue" @tab-click="handleClick">
             <el-tab-pane
               v-for="(item,$index) in AugustDutyTest"
@@ -130,52 +130,37 @@ export default {
       AugustDuty: [
         {
           name: "the first week",
-          lable: "第一周（8.3-8.9）",
+          lable: "第一周（10.9-10.11）",
           list: "王浩 贾玲雪 焦鹏 李留齐 吴登登 李垒垒 刘朝甲",
         },
         {
           name: "the second week",
-          lable: "第二周（8.10-8.16）",
+          lable: "第二周（10.12-10.18）",
           list: "景冰帅 张凌旗 焦鹏 李留齐 吴登登 李垒垒 刘朝甲",
         },
         {
           name: "The third week",
-          lable: "第三周（8.17-8.23）",
+          lable: "第三周（10.19-10.25）",
           list: "邢国华 贾玲雪 焦鹏 李留齐 吴登登 李垒垒 刘朝甲",
         },
         {
           name: "the fourth week",
-          lable: "第四周（8.24-8.30）",
+          lable: "第四周（10.26-10.31）",
           list: "吉燕丽 张凌旗  焦鹏 李留齐  吴登登  李垒垒  刘朝甲",
         },
       ],
       AugustDutyTest: [
         {
           name: "the first week",
-          lable: "第一周（8.3-8.9）",
+          lable: "第一周（10.9-10.10）",
           list: [
             {
-              date: "2020-08-03",
-              name: "吉燕丽",
-              address: "后端",
-            },
-            {
-              date: "2020-08-04",
-              name: "邢国华",
-              address: "后端",
-            },
-            {
-              date: "2020-08-05",
-              name: "贾玲雪",
-              address: "前端",
-            },
-            {
-              date: "2020-08-06",
+              date: "2020-10-09",
               name: "王浩",
               address: "后端",
             },
             {
-              date: "2020-08-07",
+              date: "2020-10-10",
               name: "景冰帅",
               address: "后端",
             },
@@ -183,30 +168,30 @@ export default {
         },
         {
           name: "the second week",
-          lable: "第二周（8.10-8.16）",
+          lable: "第二周（10.12-10.16）",
           list: [
             {
-              date: "2020-08-10",
+              date: "2020-10-12",
               name: "刘朝甲",
               address: "后端",
             },
             {
-              date: "2020-08-11",
+              date: "2020-10-13",
               name: " 李垒垒",
               address: "前端",
             },
             {
-              date: "2020-08-12",
+              date: "2020-10-14",
               name: "张凌旗",
               address: "前端",
             },
             {
-              date: "2020-08-13",
+              date: "2020-10-15",
               name: "吴登登",
               address: "测试",
             },
             {
-              date: "2020-08-14",
+              date: "2020-10-16",
               name: "王小虎",
               address: " 测试",
             },
@@ -214,30 +199,30 @@ export default {
         },
         {
           name: "The third week",
-          lable: "第三周（8.17-8.23）",
+          lable: "第三周（10.19-10.23）",
           list: [
             {
-              date: "2020-08-17",
+              date: "2020-10-19",
               name: "吉燕丽",
               address: "后端",
             },
             {
-              date: "2020-08-18",
+              date: "2020-10-20",
               name: "邢国华",
               address: "后端",
             },
             {
-              date: "2020-08-19",
+              date: "2020-10-21",
               name: "贾玲雪",
               address: "前端",
             },
             {
-              date: "2020-08-20",
+              date: "2020-10-22",
               name: "王浩",
               address: "后端",
             },
             {
-              date: "2020-08-21",
+              date: "2020-10-23",
               name: "景冰帅",
               address: "后端",
             },
@@ -245,30 +230,30 @@ export default {
         },
         {
           name: "the fourth week",
-          lable: "第四周（8.24-8.30）",
+          lable: "第四周（10.26-10.30）",
           list: [
             {
-              date: "2020-08-24",
+              date: "2020-10-26",
               name: "刘朝甲",
               address: "后端",
             },
             {
-              date: "2020-08-25",
+              date: "2020-10-27",
               name: "李垒垒",
               address: "前端",
             },
             {
-              date: "2020-08-26",
+              date: "2020-10-28",
               name: "张凌旗",
               address: "前端",
             },
             {
-              date: "2020-08-27",
+              date: "2020-10-29",
               name: "吴登登",
               address: "测试",
             },
             {
-              date: "2020-08-28",
+              date: "2020-10-30",
               name: "吴登登",
               address: "测试",
             },
